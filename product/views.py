bde1f6feb24b716021d70c533a4916f5b181c01b
Daniel Murphy <knysnacoder@gmail.com>
2022-09-07 11:23:03 +0200

    adding a product view

--- product/views.py
+++ product/views.py
@@ -1,4 +1,6 @@
+from itertools import product
 from django.shortcuts import render
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -13,4 +15,14 @@ class LatestProductList(APIView):
     serializer: ProductSerializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
     
+class ProductDetail(APIView):
+  def get_object(self, category_slug, product_slug):
+    try:
+      return Product.objects.filter(category_slug=category_slug).get(slug=product_slug)
+    except Product.DoesNotExist:
+      raise Http404
 
+  def get(self, request, category_slug, product_slug, format=None):
+    product = self.get_object(category_slug, product_slug)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
